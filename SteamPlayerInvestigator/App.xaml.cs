936be69ad3d8693b7357f98cc6040159318ba310
JONATHAN MUMMERY <johay.jjj@googlemail.com>
2022-11-23 03:10:06 +0000

    Add project files.

---
+++ SteamPlayerInvestigator/App.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SteamPlayerInvestigator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+    public class Player
+        {
+            public string steamid { get; set; }
+            public int communityvisibilitystate { get; set; }
+            public int profilestate { get; set; }
+            public string personaname { get; set; }
+            public string profileurl { get; set; }
+            public string avatar { get; set; }
+            public string avatarmedium { get; set; }
+            public string avatarfull { get; set; }
+            public string avatarhash { get; set; }
+            public int personastate { get; set; }
+            public string realname { get; set; }
+            public string primaryclanid { get; set; }
+            public int timecreated { get; set; }
+            public int personastateflags { get; set; }
+            public string loccountrycode { get; set; }
+            public string locstatecode { get; set; }
+            public int loccityid { get; set; }
+        }
+
+        public class Response
+        {
+            public List<Player> players { get; set; }
+        }
+
+        public class Root
+        {
+            public Response response { get; set; }
+        }
+}
---
+++ SteamPlayerInvestigator/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Text.Json;
+
+namespace SteamPlayerInvestigator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string steamID = "76561198054022028";
+            string url = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=7E7C3A26841681369678AE28CDF62901&steamids=" + steamID;
+            using (var wb = new WebClient())
+            {
+                var response = wb.DownloadString(url);
+                Root myDeserializedClass = JsonSerializer.Deserialize<Root>(response);
+                System.Diagnostics.Debug.WriteLine("Display Name:" + myDeserializedClass.response.players[0].personaname);
+            }
+        }
+    }
+}
